4419e7eb9dbf478f6a837eca7d6dcb77e904484a
Victor Chong <victor.chong@linaro.org>
2016-11-25 06:39:45 +0000

    try to do right thing for spidev

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -695,6 +695,8 @@ static struct class *spidev_class;
 static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv" },
 	{ .compatible = "lineartechnology,ltc2488" },
+	{ .compatible = "hisilicon,hi6220-hikey" },
+	{ .compatible = "hisilicon,hi6220" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
