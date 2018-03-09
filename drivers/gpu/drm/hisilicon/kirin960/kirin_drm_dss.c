rmi1974/git.linaro.org-john.stultz-android-dev

drm: kirin960: Add support for ARGB888 and ABGR888 pixel formats
Add support for DRM_FORMAT_ARGB888 and ABGR888 formats, which
are useful for the drm_hwcomposer.

Signed-off-by: John Stultz <john.stultz@linaro.org>

drivers/gpu/drm/hisilicon/kirin960/kirin_drm_dss.c
@@ -53,14 +53,15 @@ static const struct dss_format dss_formats[] = {
 	{ DRM_FORMAT_XBGR8888, HISI_FB_PIXEL_FORMAT_BGRX_8888 },
 	{ DRM_FORMAT_RGBA8888, HISI_FB_PIXEL_FORMAT_RGBA_8888 },
 	{ DRM_FORMAT_BGRA8888, HISI_FB_PIXEL_FORMAT_BGRA_8888 },
-	/*{ DRM_FORMAT_ARGB8888,  },*/
-	/*{ DRM_FORMAT_ABGR8888,  },*/
+	{ DRM_FORMAT_ARGB8888, HISI_FB_PIXEL_FORMAT_BGRA_8888 },
+	{ DRM_FORMAT_ABGR8888, HISI_FB_PIXEL_FORMAT_RGBA_8888 },
 };
 
 static const u32 channel_formats1[] = {
 	DRM_FORMAT_RGB565, DRM_FORMAT_BGR565,
 	DRM_FORMAT_XRGB8888, DRM_FORMAT_XBGR8888,
-	DRM_FORMAT_RGBA8888, DRM_FORMAT_BGRA8888
+	DRM_FORMAT_RGBA8888, DRM_FORMAT_BGRA8888,
+	DRM_FORMAT_ARGB8888, DRM_FORMAT_ABGR8888,
 };
 
 u32 dss_get_channel_formats(u8 ch, const u32 **formats)

vchong/linux

Change-Id: Icfb48d927323d0353034e8cc144cc073879a8074

drivers/gpu/drm/hisilicon/kirin960/kirin_drm_dss.c
@@ -53,14 +53,15 @@ static const struct dss_format dss_formats[] = {
 	{ DRM_FORMAT_XBGR8888, HISI_FB_PIXEL_FORMAT_BGRX_8888 },
 	{ DRM_FORMAT_RGBA8888, HISI_FB_PIXEL_FORMAT_RGBA_8888 },
 	{ DRM_FORMAT_BGRA8888, HISI_FB_PIXEL_FORMAT_BGRA_8888 },
-	/*{ DRM_FORMAT_ARGB8888,  },*/
-	/*{ DRM_FORMAT_ABGR8888,  },*/
+	{ DRM_FORMAT_ARGB8888, HISI_FB_PIXEL_FORMAT_ARGB_8888 },
+	{ DRM_FORMAT_ABGR8888, HISI_FB_PIXEL_FORMAT_ABGR_8888 },
 };
 
 static const u32 channel_formats1[] = {
 	DRM_FORMAT_RGB565, DRM_FORMAT_BGR565,
 	DRM_FORMAT_XRGB8888, DRM_FORMAT_XBGR8888,
-	DRM_FORMAT_RGBA8888, DRM_FORMAT_BGRA8888
+	DRM_FORMAT_RGBA8888, DRM_FORMAT_BGRA8888,
+	DRM_FORMAT_ARGB8888, DRM_FORMAT_ABGR8888,
 };
 
 u32 dss_get_channel_formats(u8 ch, const u32 **formats)